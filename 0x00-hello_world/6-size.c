e66c4a88e84877d7c6c69c7bc915920e8e16b086
aymane66 <ayman.mouhcine1995@gmail.com>
2023-02-16 23:42:25 +0100

    new commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints the size of various types on the computer it is compiled and run on
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 8 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+return (0);
+}
