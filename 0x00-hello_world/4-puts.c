5a359730df8494aca14644d78ea74e89fbbc2bbf
aymane66 <ayman.mouhcine1995@gmail.com>
2023-02-16 22:01:59 +0100

    new program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/*
+ * Main - This is a program that displays a sentence without using printf
+ * Return 0
+*/
 int main(void)
 {
-
-puts "Programming is like building a multilingual puzzle";
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
